41e42ed7b9ec27f4b12fab208876d9e9e5e343c9
tanya kachanov <chernika.lisa@mail.ru>
2019-04-25 19:04:42 +0300

    Add project files.

---
+++ SerchingOfGitHubRepositories/Controllers/BookmarksController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SerchingOfGitHubRepositories.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookmarksController : ControllerBase
+    {
+        public static List<string> Bookmarks=new List<string> ();
+
+
+        [HttpGet]
+        public bool AddRepository( [FromQuery]string url)
+        {
+            if (url == null || url.Length == 0)
+                return false;
+            if (!Bookmarks.Contains(url))
+            {
+                Bookmarks.Add(url);
+            }
+
+            return true;
+        }
+    }
+}
